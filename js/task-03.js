d4e4758a8638a264be9d9451a60448ced55ad4d5
RomanKorotun <127940013+RomanKorotun@users.noreply.github.com>
2023-07-27 21:53:13 +0300

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -14,13 +14,11 @@ const images = [
 ];
 
 const galleryList = document.querySelector('.gallery');
-console.log(galleryList);
-console.dir(galleryList);
 
 function getListItem(arr) {
   const markup = arr
     .map(({ url, alt }) => {
-      return `<li class="js-item"><img src="${url}" alt="${alt}" style = "border:2px solid green; box-shadow: 3px 5px 5px black; border-radius: 5px" width = "300" height = "auto" /></li>`;
+      return `<li class="js-item"><img src="${url}" alt="${alt}" style = "border: 2px solid green; box-shadow: 3px 5px 5px black; border-radius: 5px" width = "300" height = "200" /></li>`;
     })
     .join('');
   galleryList.insertAdjacentHTML('beforeend', markup);
