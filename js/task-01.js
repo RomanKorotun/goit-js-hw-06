159aeeb50322f4f45c416d7a558cff01c6cd5070
RomanKorotun <127940013+RomanKorotun@users.noreply.github.com>
2023-07-27 14:04:01 +0300

    task-1

---
+++ js/task-01.js
@@ -0,0 +1,34 @@
+// // 1.1)
+
+// const categories = document.querySelector('#categories');
+// const arrItems = categories.children;
+// const quantityItems = arrItems.length;
+// const resultQuantityItems = `Number of categories: ${quantityItems}`;
+// console.log(resultQuantityItems);
+
+// // 2.1)
+
+// const newArrItems = [...arrItems];
+
+// newArrItems.forEach(elem => {
+//   const category = `Category: ${elem.firstElementChild.textContent}`;
+//   const elements = `Elements: ${elem.lastElementChild.children.length}`;
+//   console.log(category);
+//   console.log(elements);
+// });
+
+// 1.1)
+
+const items = document.querySelectorAll('.js-item');
+const quantityItems = items.length;
+const resultQuantityItems = `Number of categories: ${quantityItems}`;
+console.log(resultQuantityItems);
+
+// 2.1)
+
+items.forEach(elem => {
+  const category = `Category: ${elem.firstElementChild.textContent}`;
+  const elements = `Elements: ${elem.lastElementChild.children.length}`;
+  console.log(category);
+  console.log(elements);
+});
