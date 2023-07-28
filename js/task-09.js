54b6c89a144828aab732913853ce2e7b552ef818
RomanKorotun <127940013+RomanKorotun@users.noreply.github.com>
2023-07-28 21:56:12 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const elements = {
+  btn: document.querySelector('.change-color'),
+  body: document.querySelector('body'),
+  span: document.querySelector('.color'),
+};
+
+elements.btn.addEventListener('click', handlerClick);
+
+function handlerClick() {
+  elements.body.style.backgroundColor = getRandomHexColor();
+  elements.span.textContent = getRandomHexColor();
+}
