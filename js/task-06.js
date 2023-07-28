0944571135a37b48e474509f3defe7936aeb3ae4
RomanKorotun <127940013+RomanKorotun@users.noreply.github.com>
2023-07-28 17:22:34 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,13 +1,34 @@
 const input = document.querySelector('#validation-input');
 input.addEventListener('blur', handlerInput);
 
+// function handlerInput(evt) {
+//   if (
+//     evt.currentTarget.value.length ===
+//     Number(evt.currentTarget.getAttribute('data-length'))
+//   ) {
+//     evt.currentTarget.classList.remove('invalid');
+//     evt.currentTarget.classList.add('valid');
+//     return;
+//   }
+
+//   evt.currentTarget.classList.remove('valid');
+//   evt.currentTarget.classList.add('invalid');
+// }
+
 function handlerInput(evt) {
+  console.log(evt.currentTarget.value.length);
   if (
     evt.currentTarget.value.length ===
     Number(evt.currentTarget.getAttribute('data-length'))
   ) {
     evt.currentTarget.classList.remove('invalid');
     evt.currentTarget.classList.add('valid');
+    return;
+  }
+
+  if (evt.currentTarget.value.length === 0) {
+    evt.currentTarget.classList.remove('invalid');
+    evt.currentTarget.classList.remove('valid');
     return;
   }
 
