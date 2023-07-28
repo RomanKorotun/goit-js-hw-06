558b3a1ca3a60a08f5c22c62e6ab1ffb3facdc32
RomanKorotun <127940013+RomanKorotun@users.noreply.github.com>
2023-07-28 16:48:52 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', handlerSubmit);
+
+function handlerSubmit(evt) {
+  evt.preventDefault();
+
+  const { email, password } = evt.currentTarget.elements;
+  if (email.value === '') {
+    alert('Поле email повинно бути заповнено!');
+  }
+  if (password.value === '') {
+    alert('Поле password повинно бути заповнено!');
+  }
+
+  const objForm = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log(objForm);
+
+  evt.currentTarget.reset();
+}
